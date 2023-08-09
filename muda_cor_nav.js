a862a2de44a797bc9b7084e22a02dc37855a002a
CADU110 <cadumotaalves@gmail.com>
2023-08-08 23:01:40 -0300

    maais animacoes

--- muda_cor_nav.js
+++ muda_cor_nav.js
@@ -5,6 +5,10 @@ const menuMobile = document.querySelector(".mobile_menu");
 const hometext = document.querySelector(".home-text");
 const hometext2 = document.querySelector(".home-text2")
 const eventima = document.querySelector(".eventos-ima");
+const cultosi = document.querySelector(".cultos-i");
+const cultos = document.querySelector(".culto");
+
+
 
 
 
@@ -15,6 +19,8 @@ function activeScroll() {
     hometext.classList.toggle('ativo',scrollY > 0);
     hometext2.classList.toggle('ativo',scrollY > 1600);
     eventima.classList.toggle('ativo',scrollY > 800);
+    cultosi.classList.toggle('ativo',scrollY > 2200);
+    cultos.classList.toggle('ativo',scrollY > 2300);
 }
 
 window.addEventListener('scroll', activeScroll);
