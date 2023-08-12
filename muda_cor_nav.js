273dbb69a7b7694c54ee0ddaeabd11a57bf11f9a
CADU110 <cadumotaalves@gmail.com>
2023-08-12 13:43:27 -0300

    melhorias

--- muda_cor_nav.js
+++ muda_cor_nav.js
@@ -2,11 +2,7 @@ const menu = document.querySelector('ul');
 const logo = document.querySelector('.logo');
 const navBar = document.querySelector('#nav-bar');
 const menuMobile = document.querySelector(".mobile_menu");
-const hometext = document.querySelector(".home-text");
-const hometext2 = document.querySelector(".home-text2")
-const eventima = document.querySelector(".eventos-ima");
-const cultosi = document.querySelector(".cultos-i");
-const contribuir = document.querySelector(".contribuir");
+
 
 
 
@@ -15,11 +11,6 @@ function activeScroll() {
     menu.classList.toggle('ativo', scrollY > 0);
     logo.classList.toggle('ativo', scrollY > 0);
     navBar.classList.toggle('ativo', scrollY > 0);
-    hometext.classList.toggle('ativo',scrollY > 0);
-    hometext2.classList.toggle('ativo',scrollY > 1600);
-    eventima.classList.toggle('ativo',scrollY > 800);
-    cultosi.classList.toggle('ativo',scrollY > 2200);
-    contribuir.classList.toggle('ativo',scrollY > 2300);
 }
 
 window.addEventListener('scroll', activeScroll);
