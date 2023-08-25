ce9e6538a32e3536dbf57de5bfadf025ea09acd9
CADU110 <cadumotaalves@gmail.com>
2023-08-25 17:35:55 -0300

    começo confição de fé

---
+++ js/conficao.js
@@ -0,0 +1,18 @@
+const todosOsMais = document.querySelectorAll('.mais');
+const todosOsTextos = document.querySelectorAll('.texto');
+const todosOsLine7 = document.querySelectorAll('.line7');
+
+todosOsMais.forEach((mais, index) => {
+    let clicado = false;
+    mais.addEventListener("click", () => {
+        if (!clicado) {
+            todosOsTextos[index].classList.add('animada');
+            todosOsLine7[index].classList.add('animada');
+        } else {
+            todosOsTextos[index].classList.remove('animada');
+            todosOsLine7[index].classList.remove('animada');
+        }
+        
+        clicado = !clicado;
+    });
+});
