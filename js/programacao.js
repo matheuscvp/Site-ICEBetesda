5bce2f00c9076b2319b49b2244b1695cc6182937
CADU110 <cadumotaalves@gmail.com>
2023-09-11 23:29:46 -0300

    programações

---
+++ js/programacao.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const card = document.querySelector('.card');
+    const anteriorBotao = document.getElementById('anterior');
+    const proximoBotao = document.getElementById('proximo');
+    let virarCartao = false;
+
+    const imagens = [
+        {
+            front: 'imagens/Fundos/Raw Photo.jpg',
+            back: 'imagens/Fundos/Raw Photo.jpg'
+        },
+        {
+            front: 'imagens/Fundos/fundo_ket.jpg',
+            back: 'imagens/Fundos/fotoPGS.jpg'
+        },
+        {
+            front: 'imagens/Fundos/fotoPGS.jpg',
+            back: 'imagens/Fundos/fundo_ket.jpg'
+        }
+        // Adicione mais imagens conforme necessário
+    ];
+
+    let currentImageIndex = 0;
+
+    function trocarImagens() {
+        const currentImage = imagens[currentImageIndex];
+        if (!virarCartao) {
+            card.style.transform = 'rotateY(180deg)';
+            card.querySelector('.card-front img').src = currentImage.back;
+        } else {
+            card.style.transform = 'rotateY(0deg)';
+            card.querySelector('.card-front img').src = currentImage.front;
+        }
+        virarCartao = !virarCartao;
+    }
+
+    function slideAnterior() {
+        currentImageIndex--;
+        if (currentImageIndex < 0) {
+            currentImageIndex = imagens.length - 1;
+        }
+        trocarImagens();
+    }
+
+    function proximoSlide() {
+        currentImageIndex++;
+        if (currentImageIndex >= imagens.length) {
+            currentImageIndex = 0;
+        }
+        trocarImagens();
+    }
+
+    anteriorBotao.addEventListener('click', slideAnterior);
+    proximoBotao.addEventListener('click', proximoSlide);
+});
+
+
+
